b2e178e70184aae92fc0d704eb41dabbc21d4512
Sizwe Mazibuko <smazibuko@e4.co.za>
2025-02-24 15:33:52 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'auth0-authenticator',
+  webDir: 'www'
+};
+
+export default config;
